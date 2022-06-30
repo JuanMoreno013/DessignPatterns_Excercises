11c3c11e3607704cb8a6a4bbe263e0cf95468426
Juan Moreno <jmoreno@griddynamics.com>
2022-06-30 09:24:16 -0500

    Fix the draw method for circle wrapper

--- src/main/java/decorator/Circle.java
+++ src/main/java/decorator/Circle.java
@@ -4,6 +4,6 @@ package decorator;
 public class Circle implements Shape{
     @Override
     public void draw() {
-        System.out.println("Shape: Rectangle");
+        System.out.println("Shape: Circle");
     }
 }
