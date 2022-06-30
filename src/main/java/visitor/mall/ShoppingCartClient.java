3bc7ae3a1a503e2685c537ca9a52899fbf270812
Juan Moreno <jmoreno@griddynamics.com>
2022-06-30 13:06:39 -0500

    Change the implementation for client by adding the items using a List

--- src/main/java/visitor/mall/ShoppingCartClient.java
+++ src/main/java/visitor/mall/ShoppingCartClient.java
@@ -1,20 +1,24 @@
 package visitor.mall;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCartClient {
     public static void main(String[] args) {
 
-        ItemElement[] items = new ItemElement[]{
-                new Book(20, "1234"),
-                new Book(100, "5678"),
-                new Fruit(10, 2, "Banana"),
-                new Fruit(5, 5, "Apple")
-        };
+        List<ItemElement> items = new ArrayList<>();
+
+        items.add(new Book(20, "1234"));
+        items.add(new Book(100, "5678"));
+        items.add(new Fruit(10, 2, "Banana"));
+        items.add(new Fruit(5, 5, "Apple"));
+
 
         int total = calculatePrice(items);
         System.out.println("Total Cost = "+total);
     }
 
-    private static int calculatePrice(ItemElement[] items)
+    private static int calculatePrice(List<ItemElement> items)
     {
         ShoppingCartVisitor visitor = new ShoppingCartVisitorImpl();
         int sum=0;
